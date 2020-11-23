0761ff8bd46f18308cce8c96132d5ef8b92f217e
renee212020 <71196944+renee212020@users.noreply.github.com>
2020-11-22 19:37:46 -0500

    lab10

---
+++ lab10.js
@@ -0,0 +1,21 @@
+function calc(type){
+
+var number1 = Number(document.getElementById('num1').value);
+var number2 = Number(document.getElementById('num2').value);
+
+if (type="add"){
+  document.getElementById('output').innerHtml= number1 + number2
+  }
+
+else if (type="subtract"){
+    document.getElementById('output').innerHtml= number1 - number2
+    }
+
+else if (type="multiply"){
+    document.getElementById('output').innerHtml= number1 * number2
+        }
+
+else if (type="divide"){
+      document.getElementById('output').innerHtml= number1 / number2
+            }
+}
